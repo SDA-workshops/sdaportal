a8ece3e75c697234b4c0ebea8ddae171d7e6183a
Roman Dryndik <dryndikroman@gmail.com>
2022-04-03 15:54:13 +0200

    Add app.py

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-from sqlalchemy import or_
-
+from db.model import Article, User
 from db.session import Session
-from db.model import User
 
 
 def main():
     session = Session()
-    users_query = session.query(User).filter(
-        or_(
-            User.email == "bob.marley@gmail.com",
-            User.first_name == "Robert",
-            User.first_name == "Mike"
-        )
+    article_1 = session.query(Article).filter(Article.id == 1).one()
+    print(article_1.author)
+
+    article_2 = session.query(Article).filter(Article.id == 3).one()
+    print(article_2.author)
+
+    user = session.query(User).filter_by(id=201).one()
+    print(user.articles)
+
+    user.articles.append(
+        Article(title="Cute panda #2", content="...")
     )
-    for user in users_query:
-        print(user)
+    session.commit()
+
+    print(user.articles)
 
 
 if __name__ == "__main__":
